bb502f22a70581c472020c8939027ca9c1389960
Jaspreet <jassisanghera408@gmail.com>
2020-11-03 13:24:04 -0800

    initial commit

---
+++ app1.js
@@ -0,0 +1,51 @@
+// Frequency Counter -- given two strings write a function to determine if the second string is anagram of first. Such as cinema and iceman
+
+
+function same(first,second){
+
+    // check if the length is not same of both strings then return false.
+
+    if(first.length !== second.length){
+        return false;
+    }  
+    // created an object to count the number of times the letter came.
+    let obj = {};
+    for(let i = 0 ;i<first.length;i++){
+        key = first[i];
+        if (obj[key] === undefined){
+            obj[key] = 1;
+        }
+        else{
+            obj[key]++;
+        }
+    }
+
+    // created another obj to count the number of times the letter came
+
+    let obj1 = {};
+    for(let i = 0 ;i<second.length;i++){
+        key = second[i];
+        if (obj1[key] === undefined){
+            obj1[key] = 1;
+        }
+        else{
+            obj1[key]++;
+        }
+    }
+    // console.log(obj);
+    // console.log(obj1);
+    // check if first str has the same number of times a letter coming in str2
+
+    for(let i = 0; i<first.length; i++){
+        key = second[i];
+        if(!(key in obj)){
+            return false;
+        }
+        if(obj[key] !== obj1[key]){
+            return false;
+        }
+        return true;
+    }
+    
+ }
+ same('ab','ab');
