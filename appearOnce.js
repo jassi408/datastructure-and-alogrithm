7e3402ddfb725a3a344aa9d72e549f71c89b030b
Jaspreet <jassisanghera408@gmail.com>
2020-11-04 13:23:06 -0800

    appearOnce added

---
+++ appearOnce.js
@@ -0,0 +1,3 @@
+// given an array of integers in which two element appear exactly once and all other element appear twice. find two element that appear once.
+// for example --- appearOnce([2,4,6,8,10,2,6,10]) return 4 and 8
+
