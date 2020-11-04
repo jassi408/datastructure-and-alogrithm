3116f0bf7aa1e37b6041396e36ddc626076589b5
Jaspreet <jassisanghera408@gmail.com>
2020-11-04 13:28:11 -0800

    appearOnce updated

--- appearOnce.js
+++ appearOnce.js
@@ -1,3 +1,30 @@
 // given an array of integers in which two element appear exactly once and all other element appear twice. find two element that appear once.
 // for example --- appearOnce([2,4,6,8,10,2,6,10]) return 4 and 8
 
+// frequency Counter pattern
+
+function appearOnce(arr){
+    let result = [];
+    if(arr.length <= 1){
+        return null;
+    }else if((arr.length === 2) && (arr[0] !== arr[1])){
+        return arr[0],arr[1];
+    }
+    let obj = {};
+    for(let i = 0; i < arr.length ; i++){
+        let num = arr[i];
+        obj[num] ? obj[num]++ : obj[num] = 1;
+     }
+     console.log(obj);
+
+     for(let i =0 ; i< arr.length ; i++){
+         let num = arr[i];
+         if(obj[num] === 1){
+             result.push(num);
+         }
+     }
+     return result;
+}
+
+appearOnce([2,4,6,8,10,2,6,10]);
+
