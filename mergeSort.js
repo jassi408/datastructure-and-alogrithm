86f6784aa5c999eb2eb351cee05e98173e5b4bd1
Jaspreet Kaur <jassi_sanghera@yahoo.com>
2020-12-02 13:27:45 -0800

    new file added

--- mergeSort.js
+++ mergeSort.js
@@ -1,4 +1,4 @@
-function mergeSort(arr1,arr2){
+function merge(arr1,arr2){
     let result  = [];
     let i = 0;
     let j = 0;
@@ -14,13 +14,20 @@ function mergeSort(arr1,arr2){
         }
     }
     while(i < arr1.length ){
-            result.push(arr2[j]);
-            j++;
-        }
-     while(j < arr2.length){
             result.push(arr1[i]);
             i++;
         }
+     while(j < arr2.length){
+            result.push(arr2[j]);
+            j++;
+        }
     return result;
 }
-mergeSort([1,10,50,101],[2,14,99,100]);
+function mergeSort(arr){
+    if(arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length/2);
+    let left = mergeSort(arr.slice(0,mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left,right);
+}
+mergeSort([9,4,6,7,2,3,4,5,8,4])
